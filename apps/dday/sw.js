6c47447e1a83d6b24c06b8b45c7ff7f4d35f25a1
loraticon-test <loraticon.test@gmail.com>
2026-08-12 07:52:36 +0900

    Add files via upload

--- apps/dday/sw.js
+++ apps/dday/sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = 'cozy-dday-v15';
+const CACHE_NAME = 'cozy-dday-v16';
 const APP_SHELL = [
   './index.html',
   './manifest-dday.webmanifest',
   '../../assets/js/install-config.js?v=20260811-0001',
   '../../assets/js/dday-app.js?v=20260811-0015',
-  '../../assets/css/dday.css?v=20260811-0015'
+  '../../assets/css/dday.css?v=20260812-0016'
 ];
 const CACHEABLE_HOSTS = new Set(['cdn.jsdelivr.net']);
 
